d8f2f24369a8178786bce3610ada2a57daa28795
Falzafar1 <161683671+Falzafar1@users.noreply.github.com>
2024-03-05 10:32:39 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
  *   Hari dan Tanggal    : Senin, 4 Maret 2024
  *   Nama (NIM)          : Naufal Afiq Muzaffar (13222025)
  *   Nama File           : main.c
- *   Deskripsi           : 
+ *   Deskripsi           : Menampilkan faktorisasi prima dari suatu bilangan C. Bilangan C tersebut merupakan
+ *                         penjumlahan dari 2 bilangan lain yang dibalik urutannya.  
  */
 
 #include <stdio.h>
@@ -13,11 +14,10 @@
 // Fungsi untuk membalikkan representasi bilangan bulat
 int reverseBilangan(int* bilangan) {
 
-    int Xaksen;
+    int i=0;
     int kurang0=0;
     char strX[15]={};
     char strX2[15]={};
-    int i=0;
     int kondisi = 0;
 
     sprintf(strX, "%d", *bilangan);
@@ -28,43 +28,52 @@ int reverseBilangan(int* bilangan) {
         }
         else{
             kondisi = 1;
-        }
-        
+        }  
     }
 
     for ( i = 0; i < (strlen(strX)-kurang0); i++){
         strX2[i] = strX[strlen(strX)-kurang0-i-1];
     }
-
     *bilangan = atoi(strX2);
 
 }
 
 // Fungsi untuk mendapatkan faktorisasi prima dari suatu bilangan
-// void faktorisasiPrima(int *angka) {
-//     // isi dengan program untuk mencari faktorisasi prima dari C
-//     // silahkan lengkapi parameter fungsi ini
-    
-// }
-
-int main() {
-    int A;
-    int B;
-    int C;
-    int k;
-    int j;
-    int kondisi1 =0;
-    int kondisi2 = 0;
+void faktorisasiPrima(int *angka) {
+    int j=0;
     int primaterbesar;
     int cekprima = 0;
+    
+        for (j=2; j<(*angka)+1; j++){
+            if (((*angka)%j)==0){
+                cekprima +=1;
+            }
+            if ((cekprima == 1) && (j>=2) && (j<=*angka) && ((*angka%j)==0)){
+                primaterbesar = j;
+                *angka = *angka/primaterbesar;
+                if (*angka!=1){
+                    printf("%d ", primaterbesar);
+                    printf("x ");
+                }
+                else{
+                    printf("%d", primaterbesar); 
+                }
+            }
+        }
+}
+
+int main() {
+    int A,B,C;
+    int kondisi1 =0;
+    int kondisi2 = 0;
 
     // Meminta input dari pengguna
-
     printf("Masukkan bilangan A: ");
     scanf("%d", &A);
     printf("Masukkan bilangan B: ");
     scanf("%d", &B);
 
+    // Melakukan perulangan untuk menerima input s
     while (kondisi1 ==0 || kondisi2 ==0){
         if (A <=0){
             printf ("\nBilangan yang Anda masukkan bernilai 0\n");
@@ -85,46 +94,18 @@ int main() {
         }
     }
 
-
     // Membalik representasi desimal A dan B menggunakan pointer
     reverseBilangan(&A);
     reverseBilangan(&B);
 
-    // printf("%d\n", A);
-    // printf("%d\n", B);
     // Menjumlahkan A' dan B' untuk mendapatkan C
     C = A + B;
     printf("\nBilangan C adalah %d\n",C);
     printf("Faktorisasi prima dari %d = ", C);
 
-    int i=j=0;
-
-    while (C!=1){
-        j=0;
-        for (j=2; j<C+1; j++){
-            if (((C)%j)==0){
-                cekprima +=1;
-            }
-            if ((cekprima == 1) && (j>=2) && (j<=C) && ((C%j)==0)){
-                primaterbesar = j;
-            }
-
-            if (j == C){
-                C = C/primaterbesar;
-                if (C!=1){
-                    printf("%d ", primaterbesar);
-                    printf("x ");
-                }
-                else{
-                    printf("%d", primaterbesar); 
-                }
-            }
-        }
-        cekprima=0;
-
-    }
-
-
     // Mendapatkan faktorisasi prima dari C menggunakan pointer
-return 0;
+    while (C!=1){
+        faktorisasiPrima(&C);
+    }
+    return 0;
 }
